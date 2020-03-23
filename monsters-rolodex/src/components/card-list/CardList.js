c7b4e5d57473d4a69063f8eebb81cc19102442b8
Sebastian <js.diazr@uniandes.edu.co>
2020-03-23 13:25:01 -0500

    Card and CardList components

--- monsters-rolodex/src/components/card-list/CardList.js
+++ monsters-rolodex/src/components/card-list/CardList.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { Card } from "../card/Card";
+import "./CardList.css";
 
 export const CardList = props => {
-  console.log(props);
-  return <div>Hello</div>;
+  return (
+    <div className="card-list">
+      {props.monsters.map(monster => (
+        <Card key={monster.id} monster={monster} />
+      ))}
+    </div>
+  );
 };
