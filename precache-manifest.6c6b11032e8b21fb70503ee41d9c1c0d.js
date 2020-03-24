89407e2988d3d36c7005367a7debb22cd042e302
Sebastian <js.diazr@uniandes.edu.co>
2020-03-23 21:04:58 -0500

    Updates

---
+++ precache-manifest.6c6b11032e8b21fb70503ee41d9c1c0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30296553cbf9f453797ce3868e395b25",
+    "url": "/MonsterRolodex/index.html"
+  },
+  {
+    "revision": "01834a10a9f66335d789",
+    "url": "/MonsterRolodex/static/css/main.7871f563.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/MonsterRolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonsterRolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01834a10a9f66335d789",
+    "url": "/MonsterRolodex/static/js/main.78131329.chunk.js"
+  },
+  {
+    "revision": "35754fcbc87d98e43b53",
+    "url": "/MonsterRolodex/static/js/runtime-main.751de5f8.js"
+  }
+]);
