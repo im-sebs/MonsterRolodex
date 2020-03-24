2b91d2089546ed37c32249ad2caa8d0742521548
Sebastian <js.diazr@uniandes.edu.co>
2020-03-23 21:00:32 -0500

    Finished

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/CardList";
+import { SearchBox } from "./components/search-box/SearchBox";
 import "./App.css";
 
 class App extends Component {
   state = {
-    monsters: []
+    monsters: [],
+    searchField: ""
   };
 
+  //Methods
+  handleChange = e => this.setState({ searchField: e.target.value });
+
+  //Lifecycle Methods
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
@@ -14,9 +20,19 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="Search Monster"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
